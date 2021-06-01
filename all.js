a734e2990d8de33192c64ebef466ea3494720602
EpicKnarvik97 <kristian.knarvik@knett.no>
2021-06-01 22:01:30 +0200

    Prevents the template from displaying as a mod
    This commit adds a small check which skips the template JSON when creating the mod list.

--- all.js
+++ all.js
@@ -108,6 +108,7 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         JSON.parse(xhttp.responseText).tree.forEach(item => modJSONS.push(item.path))
         modJSONS.forEach(item => {
+            if (item === "template.json") return;
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
